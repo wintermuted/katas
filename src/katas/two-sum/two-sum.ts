030ab0159abee4445ca6a9ba8a705835417efe2c
wintermuted <jameson.nyeholt@gmail.com>
2022-02-19 09:12:23 -0800

    two sum: add another test case, and fix bug

--- src/katas/two-sum/two-sum.ts
+++ src/katas/two-sum/two-sum.ts
@@ -2,18 +2,18 @@ export function twoSum(nums: number[], target: number): number[] {
   let targetIndices: number[] = [];
 
   nums.forEach((value, firstIndex) => {
-      const nextIndex = firstIndex + 1;
+      const nextIndex = firstIndex + 1
       const remainderArray = nums.slice(nextIndex)
 
         remainderArray.forEach((secondValue, secondIndex) => {
           if (targetIndices.length < 2) {
-            const result = value + secondValue;
+            const result = value + secondValue
 
             if (result === target) {
-              targetIndices.push(firstIndex, nextIndex)
+              targetIndices.push(firstIndex, nextIndex + secondIndex)
             }
-        }
-      });
+          }
+        });
   });
 
   return targetIndices;
